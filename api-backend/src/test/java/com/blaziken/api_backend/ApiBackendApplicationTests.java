52e427343de21fafb51a70417963352de771d4bd
Rhyan.Dev <rhyanlima.dev@gmail.com>
2024-10-31 00:11:38 -0300

    Inicio do projeto blaziken

---
+++ api-backend/src/test/java/com/blaziken/api_backend/ApiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blaziken.api_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
